be794cd5c703a1124c65ce517b6d43ded6654c3f
jokhero <78802573+jokhero@users.noreply.github.com>
2022-06-30 12:11:31 +0800

    1

---
+++ pages/user_main/user_main.js
@@ -0,0 +1,207 @@
+import {
+    Factory
+} from "../../class/Factory"
+
+// pages/user_main/user_main.js
+Page({
+    //点击改变按钮状态
+    wt_change() {
+        if (this.data.type_wt === 'default') {
+            this.setData({
+                type_wt: 'primary'
+            })
+        } else {
+            this.setData({
+                type_wt: 'default',
+            })
+        }
+    },
+
+    cx_change() {
+        if (this.data.type_cx === 'default') {
+            this.setData({
+                type_cx: 'primary'
+            })
+        } else {
+            this.setData({
+                type_cx: 'default',
+            })
+        }
+    },
+
+    dy_change() {
+        if (this.data.type_dy === 'default') {
+            this.setData({
+                type_dy: 'primary'
+            })
+        } else {
+            this.setData({
+                type_dy: 'default',
+            })
+        }
+    },
+
+
+
+    online_change() {
+        if (this.data.type_online === 'default') {
+            this.setData({
+                type_online: 'primary'
+            })
+        } else {
+            this.setData({
+                type_online: 'default',
+            })
+        }
+    },
+
+    offline_change() {
+        if (this.data.type_offline === 'default') {
+            this.setData({
+                type_offline: 'primary'
+            })
+        } else {
+            this.setData({
+                type_offline: 'default',
+            })
+        }
+    },
+
+    overdue_change() {
+        if (this.data.type_ongoing === '未过期') {
+            this.setData({
+                type_ongoing: '过期'
+            })
+        } else {
+            this.setData({
+                type_ongoing: '未过期',
+            })
+        }
+    },
+
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        type_wt: 'default',
+        type_dy: 'default',
+        type_cx: 'default',
+        type_online: 'default',
+        type_offline: 'default',
+        type_ongoing: '未过期',
+        start_date: '',
+        end_date: '',
+        activityList: new Array(),
+        year: new Array(),
+        month: new Array(),
+        day: new Array(),
+        tagList: new Array()
+    },
+
+    query() {
+        //如果线上线下都没选中就不传这两个参数
+        if (this.data.type_offline === 'default' && this.data.type_online === 'default') {
+            if (this.data.type_cx === 'default') {
+                var typecx = false;
+            } else {
+                var typecx = true;
+            }
+            if (this.data.type_wt === 'default') {
+                var typewt = false;
+            } else {
+                var typewt = true;
+            }
+            if (this.data.type_dy === 'default') {
+                var typedy = false;
+            } else {
+                var typedy = true;
+            }
+            if (typewt === false && typedy === false && typecx === false) {
+                var typesy = true;
+            } else {
+                var typesy = false;
+            }
+            var that = this;
+            Factory.getUser().queryActivity({
+                scoreType_cx: typecx,
+                scoreType_dy: typedy,
+                scoreType_wt: typewt,
+                scoreType_all: typesy,
+                start_date: new Date(this.data.start_date),
+                end_date: new Date(this.data.end_date)
+            })
+        }
+    },
+
+    bindstartDateChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            start_date: e.detail.value
+        })
+    },
+
+    bindendDateChange: function (e) {
+        console.log('picker发送选择改变，end携带值为', e.detail.value)
+        this.setData({
+            end_date: e.detail.value
+        })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})

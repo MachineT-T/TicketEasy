4fc9d43cd0c949e52728a66d72f70c4a823fc738
jokhero <78802573+jokhero@users.noreply.github.com>
2022-06-30 10:54:02 +0800

    线上线下检测查询页面完善

--- pages/user_main/user_main.js
+++ pages/user_main/user_main.js
@@ -1,116 +1,139 @@
 // pages/user_main/user_main.js
 Page({
     //点击改变按钮状态
-    wt_change(){
-        if(this.data.type_wt==='default'){
-             this.setData({
-                type_wt:'primary'
-            })
+    wt_change() {
+        if (!this.data.lock) {
+            if (this.data.type_wt === 'default') {
+                this.setData({
+                    type_wt: 'primary'
+                })
+            } else {
+                this.setData({
+                    type_wt: 'default',
+                })
+            }
         }
-        else{
+    },
+
+    cx_change() {
+        if (!this.data.lock) {
+            if (this.data.type_cx === 'default') {
+                this.setData({
+                    type_cx: 'primary'
+                })
+            } else {
+                this.setData({
+                    type_cx: 'default',
+                })
+            }
+        }
+    },
+
+    dy_change() {
+        if (!this.data.lock) {
+            if (this.data.type_dy === 'default') {
+                this.setData({
+                    type_dy: 'primary'
+                })
+            } else {
+                this.setData({
+                    type_dy: 'default',
+                })
+            }
+        }
+    },
+
+    sy_change() {
+        if (this.data.type_sy === 'default') {
             this.setData({
-                type_wt:'default',
+                type_sy: 'primary',
+                type_wt: 'default',
+                type_cx: 'default',
+                type_dy: 'default',
+                lock: true,
+            })
+        } else {
+            this.setData({
+                type_sy: 'default',
+                lock: false,
             })
         }
     },
 
-    cx_change(){
-        if(this.data.type_cx==='default'){
-             this.setData({
-                type_cx:'primary'
-            })
-        }
-        else{
+    online_change() {
+        if (this.data.type_online === 'default') {
             this.setData({
-                type_cx:'default',
+                type_online: 'primary'
+            })
+        } else {
+            this.setData({
+                type_online: 'default',
             })
         }
     },
 
-    dy_change(){
-        if(this.data.type_dy==='default'){
-             this.setData({
-                type_dy:'primary'
-            })
-        }
-        else{
+    offline_change() {
+        if (this.data.type_offline === 'default') {
             this.setData({
-                type_dy:'default',
+                type_offline: 'primary'
+            })
+        } else {
+            this.setData({
+                type_offline: 'default',
             })
         }
     },
 
-    sy_change(){
-        if(this.data.type_sy==='default'){
-             this.setData({
-                type_sy:'primary'
-            })
-        }
-        else{
+    overdue_change() {
+        if (this.data.type_overdue === '未过期') {
             this.setData({
-                type_sy:'default',
+                type_overdue: '过期'
+            })
+        } else {
+            this.setData({
+                type_overdue: '未过期',
             })
         }
     },
 
-    online_change(){
-        if(this.data.type_online==='default'){
-             this.setData({
-                type_online:'primary'
-            })
-        }
-        else{
-            this.setData({
-                type_online:'default',
-            })
-        }
-    },
 
-    offline_change(){
-        if(this.data.type_offline==='default'){
-             this.setData({
-                type_offline:'primary'
-            })
-        }
-        else{
-            this.setData({
-                type_offline:'default',
-            })
-        }
-    },
-
-    overdue_change(){
-        if(this.data.type_overdue==='default'){
-             this.setData({
-                type_overdue:'primary'
-            })
-        }
-        else{
-            this.setData({
-                type_overdue:'default',
-            })
-        }
-    },
-    
-    
     /**
      * 页面的初始数据
      */
     data: {
-        type_wt:'default',
-        type_dy:'default',
-        type_cx:'default',
-        type_sy:'default',
-        type_online:'default',
-        type_offline:'default',
-        type_overdue:'default'
+        type_wt: 'default',
+        type_dy: 'default',
+        type_cx: 'default',
+        type_sy: 'default',
+        type_online: 'default',
+        type_offline: 'default',
+        type_overdue: '未过期',
+        lock: false,
+        start_date: '',
+        end_date: '',
+        activityList: new Array()
     },
+
+    bindstartDateChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            start_date: e.detail.value
+        })
+    },
+
+    bindendDateChange: function (e) {
+        console.log('picker发送选择改变，end携带值为', e.detail.value)
+        this.setData({
+            end_date: e.detail.value
+        })
+    },
+
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        
+
     },
 
     /**
